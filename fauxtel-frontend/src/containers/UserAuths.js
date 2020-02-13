56fb33f030ad892bad6b31c8d19adfdda65d4d17
Tsunamins <reillyamr@gmail.com>
2020-02-13 15:24:51 -0500

    add general idea of token auth to frontend

---
+++ fauxtel-frontend/src/containers/UserAuths.js
@@ -0,0 +1,67 @@
+import React, {useState, useEffect} from 'react';
+import Header from '../components/Header'
+import SignInForm from '../components/SignInForm';
+import LoginForm from '../components/LoginForm'
+
+function UserAuths() {
+  const [user, setUser] = useState({})
+  const [form, setForm] = useState("")
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+    if(token){
+      fetch(`http://localhost:3000/auto_login`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(resp => resp.json())
+      .then(data => {
+        setUser(data)
+        // console.log(data)
+      })
+    }
+  }, [])
+
+  const handleLogin = (user) => {
+    setUser(user)
+  }
+
+  const handleFormSwitch = (input) => {
+    setForm(input)
+  }
+
+  const handleAuthClick = () => {
+    const token = localStorage.getItem("token")
+    fetch(`http://localhost:3000/user_is_authed`, {
+      headers: {
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data))
+  }
+
+  console.log(user)
+
+  const renderForm = () => {
+    switch(form){
+      case "login":
+        return <LoginForm handleLogin={handleLogin}/>
+        break;
+      default:
+        return <SignInForm handleLogin={handleLogin}/>
+    }
+  }
+  return (
+    <div className="App">
+        <Header handleFormSwitch={handleFormSwitch}/>
+        {
+          renderForm()
+        }
+        <button onClick={handleAuthClick} className="ui button">Access Authorized Route</button>
+    </div>
+  );
+}
+
+export default UserAuths;
