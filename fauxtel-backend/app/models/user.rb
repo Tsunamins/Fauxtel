0d730144a2b4d5e8ea03a1e4a41ab1ae4c3b1e5d
Tsunamins <reillyamr@gmail.com>
2020-02-13 14:12:22 -0500

    add basic validations, auth, relationship

--- fauxtel-backend/app/models/user.rb
+++ fauxtel-backend/app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    has_many :reservations
+    has_secure_password
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+
 end
