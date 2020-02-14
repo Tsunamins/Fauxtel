510257a8ded4426a7b55c0150f20af82faf900da
Tsunamins <reillyamr@gmail.com>
2020-02-14 15:55:00 -0500

    add users show

--- fauxtel-backend/config/routes.rb
+++ fauxtel-backend/config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
   #resources :users
-  resources :users, only: [:create, :update, :index]
+  resources :users, only: [:create, :update, :index, :show]
 
-  post "/login", to: "auth#login"
-   get "/auto_login", to: "auth#auto_login"
-   get "/user_authorized", to: "auth#user_authorized"
+  # post "/login", to: "auth#login"
+  #  get "/auto_login", to: "auth#auto_login"
+  #  get "/user_authorized", to: "auth#user_authorized"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
